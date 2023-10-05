ee23619ca6491ff04ceb3c88901e836c7742cc22
Strong-Tea <aleksey_borisevich@inbox.ru>
2023-10-05 13:31:58 +0300

    last version

---
+++ README.md
@@ -0,0 +1,12 @@
+# Задание
+Требуется разработать приложение с графическим интерфейсом, генерирующее на основе введенного текста QR-code и поддерживающее считывание кода как латиницы, так и русского языка. В случае считывателя, содержимое, если является ссылкой или текстом можно или открыть в браузере или скопировать.
+
+
+
+### Пример выполнения программы
+
+
+![qr1](https://github.com/Strong-Tea/QR_Scanner_Generator/assets/135996451/3a05dbb4-bdf0-48b4-9ea3-29c0c153ecc8)
+![qr2](https://github.com/Strong-Tea/QR_Scanner_Generator/assets/135996451/8e8bea5e-b246-403a-91b6-041e06d2f76f)
+![qr3](https://github.com/Strong-Tea/QR_Scanner_Generator/assets/135996451/5ecf2fc0-68bd-405c-9dd2-8b072019422d)
+![qr4](https://github.com/Strong-Tea/QR_Scanner_Generator/assets/135996451/5d869a1a-d6dc-41f6-9470-1d03a627e7e6)
---
+++ app/src/main/java/com/bsuir/qrscangenerate/MainActivity.java
@@ -0,0 +1,73 @@
+package com.bsuir.qrscangenerate;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bsuir.myapplication.R;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_scan;
+    private Button btn_generator;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_scan = findViewById(R.id.btn_scan);
+        btn_generator = findViewById(R.id.btn_generator);
+
+        btn_scan.setOnClickListener(v->
+        {
+            scanCode();
+        });
+
+        btn_generator.setOnClickListener(v->
+        {
+            Intent intent = new Intent(MainActivity.this, Generator.class);
+            startActivity(intent);
+        });
+    }
+
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash  on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result ->
+    {
+       if (result.getContents() != null) {
+           //save to phone buffer
+           ClipboardManager clipboard = (ClipboardManager) getApplicationContext().getSystemService(Context.CLIPBOARD_SERVICE);
+           ClipData clip = ClipData.newPlainText("", result.getContents().toString());
+           clipboard.setPrimaryClip(clip);
+
+           AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+           builder.setTitle("Result");
+           builder.setMessage(result.getContents());
+           builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+               @Override
+               public void onClick(DialogInterface dialog, int which) {
+                   dialog.dismiss();
+               }
+           }).show();
+       }
+    });
+}
